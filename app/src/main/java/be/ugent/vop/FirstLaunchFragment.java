8b383e236b7eed33365b5bc7a7188e8be1d1a461
Jonas <jonas.spanoghe@hotmail.com>
2015-03-02 20:40:55 +0100

    Finish checkIn at Foursquare Set tutorialFragment right again

--- app/src/main/java/be/ugent/vop/FirstLaunchFragment.java
+++ app/src/main/java/be/ugent/vop/FirstLaunchFragment.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import be.ugent.vop.foursquare.FoursquareAPI;
 import be.ugent.vop.ui.main.MainActivity;
 
 public class FirstLaunchFragment extends Fragment {
@@ -22,8 +21,6 @@ public class FirstLaunchFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,11 +30,10 @@ public class FirstLaunchFragment extends Fragment {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //FoursquareAPI.get(getActivity()).checkIn("4b7e4c44f964a520b8e82fe3");
-                //SharedPreferences settings = getActivity().getSharedPreferences(getString(R.string.sharedprefs), 0);
-                //SharedPreferences.Editor editor = settings.edit();
-                //editor.putBoolean(getString(R.string.first_launch), false);
-                //editor.apply();
+                SharedPreferences settings = getActivity().getSharedPreferences(getString(R.string.sharedprefs), 0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putBoolean(getString(R.string.first_launch), false);
+                editor.apply();
                 Intent main = new Intent(getActivity(), MainActivity.class);
                 startActivity(main);
                 getActivity().finish();
